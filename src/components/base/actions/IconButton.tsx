e4e795e4553285a3eda0966dd708c3b89e68af86
Denis Junior <junior.denis2354@outlook.com>
2023-01-13 20:27:30 -0300

    create: IconButton a base component

---
+++ src/components/base/actions/IconButton.tsx
@@ -0,0 +1,31 @@
+import { Button } from "../../plastic/action/Button";
+
+type IconButtonProps = {
+  Icon: () => JSX.Element;
+  hasBorder?: boolean;
+  size?: "small" | "medium" | "large" | "xLarge";
+  borderColor?: string;
+};
+
+const IconButtonSize = {
+  small: "1rem",
+  medium: "2rem",
+  large: "3rem",
+  xLarge: "4rem",
+};
+export const IconButton = ({
+  Icon,
+  borderColor,
+  size = "medium",
+}: IconButtonProps) => {
+  return (
+    <Button
+      width={IconButtonSize[size]}
+      height={IconButtonSize[size]}
+      backgroundColor="transparent"
+      border={Boolean(borderColor) ? `1px solid ${borderColor}` : undefined}
+    >
+      <Icon />
+    </Button>
+  );
+};
