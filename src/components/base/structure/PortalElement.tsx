cd02bd705a1cd4bb317eae621b352f1aa489ba2a
Denis Junior <junior.denis2354@outlook.com>
2023-01-13 20:36:09 -0300

    create: base component portalElement

---
+++ src/components/base/structure/PortalElement.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import { Card } from "../../plastic/structure/Card";
+
+type ModalProps = { children: React.ReactNode };
+
+export const PortalElement = ({ children }: ModalProps) => {
+  const containerRef = useRef(document.createElement("div"));
+
+  useEffect(() => {
+    document.body.appendChild(containerRef.current);
+    return () => {
+      document.body.removeChild(containerRef.current);
+    };
+  }, []);
+
+  return ReactDOM.createPortal(children, containerRef.current);
+};
