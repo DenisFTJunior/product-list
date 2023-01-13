c17de4e9325cee1263058120be78e74d5b576fcb
Denis Junior <junior.denis2354@outlook.com>
2023-01-13 20:36:48 -0300

    refactor: button to receive children instead of label

--- src/components/plastic/action/Button/index.tsx
+++ src/components/plastic/action/Button/index.tsx
@@ -1,11 +1,11 @@
 import { ButtonHTMLAttributes } from "react";
 import { StyledButton, StyleProps } from "./StyledButton";
 
-export type ButtonProps = { label?: string; icon: JSX.Element } & Partial<
+export type ButtonProps = Partial<
   Omit<ButtonHTMLAttributes<HTMLButtonElement>, "style"> & StyleProps
 >;
 
-export const Button = ({ onClick, icon, label, ...rest }: ButtonProps) => {
+export const Button = ({ onClick, children, ...rest }: ButtonProps) => {
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     if (onClick) {
       event.preventDefault();
@@ -13,5 +13,9 @@ export const Button = ({ onClick, icon, label, ...rest }: ButtonProps) => {
     }
   };
 
-  return <StyledButton onClick={handleClick} {...rest}></StyledButton>;
+  return (
+    <StyledButton onClick={handleClick} {...rest}>
+      {children}
+    </StyledButton>
+  );
 };
