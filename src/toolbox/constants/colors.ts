031501e32c6fc22931b75f16ef267cbb87676f69
Denis Junior <junior.denis2354@outlook.com>
2023-01-13 19:21:53 -0300

    create: const file for colors

---
+++ src/toolbox/constants/colors.ts
@@ -0,0 +1,6 @@
+export const WHITE = "#FFFFFF";
+export const GREEN = "#57B8B6";
+export const BLUE = "#34446D";
+
+export const BLUE_GREEN = "linear-gradient(180deg, #34446D 0%, #57B8B6 100%)";
+export const GREEN_BLUE = "linear-gradient(180deg, #57B8B6 0%, #34446D 100%)";
