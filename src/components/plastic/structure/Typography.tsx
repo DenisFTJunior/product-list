f73ed1867c62e661589109c3ecbe87fc73103f1d
Denis Junior <junior.denis2354@outlook.com>
2023-01-14 15:02:25 -0300

    refactor: can edit color of typography

--- src/components/plastic/structure/Typography.tsx
+++ src/components/plastic/structure/Typography.tsx
@@ -1,22 +1,25 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { CSSProperties } from "react";
+import { ALMOST_BLACK } from "../../../toolbox/constants/colors";
 
 type TypographyProps = {
   element: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
   text: string;
-} & Partial<Pick<CSSProperties, "fontSize" | "fontWeight">>;
+} & Partial<Pick<CSSProperties, "fontSize" | "fontWeight" | "color">>;
 
 export const Typography = ({
   element,
   text,
   fontSize,
   fontWeight,
+  color,
 }: TypographyProps) => {
   const Element = element as keyof JSX.IntrinsicElements;
   const style = css({
     fontSize: fontSize || "1rem",
     fontWeight: fontWeight || "normal",
+    color: color || ALMOST_BLACK,
   });
   return <Element css={style}>{text}</Element>;
 };
