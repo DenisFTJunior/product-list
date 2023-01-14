be8d349eb8e38ec0100760be1c3d2fa7583cd077
Denis Junior <junior.denis2354@outlook.com>
2023-01-14 14:04:45 -0300

    create: addButton

---
+++ src/components/base/actions/AddButton.tsx
@@ -0,0 +1,13 @@
+import { MdOutlineAdd } from "react-icons/md";
+import { ALMOST_WHITE, GREEN } from "../../../toolbox/constants/colors";
+import { IconButton } from "../../plastic/action/IconButton";
+
+export const AddButton = () => {
+  return (
+    <IconButton
+      Icon={() => <MdOutlineAdd color={ALMOST_WHITE} />}
+      backgroundColor={GREEN}
+      onClick={() => console.log("Add")}
+    />
+  );
+};
