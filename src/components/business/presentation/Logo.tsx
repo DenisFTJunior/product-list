0d0934af8cdcbe67472f30684dae1f58d528d327
Denis Junior <junior.denis2354@outlook.com>
2023-01-14 15:01:59 -0300

    create: logo component

---
+++ src/components/business/presentation/Logo.tsx
@@ -0,0 +1,12 @@
+import { ALMOST_WHITE } from "../../../toolbox/constants/colors";
+import { Typography } from "../../plastic/structure/Typography";
+
+export const Logo = ({ whiteLogo }: { whiteLogo?: boolean }) => (
+  <Typography
+    element="h2"
+    text="LOGO"
+    fontWeight={900}
+    fontSize="2rem"
+    color={whiteLogo ? ALMOST_WHITE : undefined}
+  />
+);
