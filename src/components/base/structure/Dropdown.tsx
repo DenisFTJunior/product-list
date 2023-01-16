795c9acab6ff96e046d3c129f2f2fc82af450e82
Denis Junior <junior.denis2354@outlook.com>
2023-01-16 14:11:25 -0300

    create: dropdown component

---
+++ src/components/base/structure/Dropdown.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Position } from "../../plastic/layout/Position";
+import { Card } from "../../plastic/structure/Card";
+
+type DropdownProps = {
+  actionElement: React.ReactElement;
+  actionType?: "onClick" | "onHover";
+  content: React.ReactElement;
+};
+
+export const Dropdown = ({
+  actionElement,
+  actionType = "onHover",
+  content,
+}: DropdownProps) => {
+  const [open, setOpen] = React.useState(false);
+  const dropdownRef = React.useRef<HTMLDivElement>(null);
+  const actionTrigger = actionType === "onHover" ? "onMouseEnter" : actionType;
+
+  React.useEffect(() => {
+    open && dropdownRef.current?.focus();
+  }, [open]);
+
+  return (
+    <Position position="relative">
+      {React.cloneElement(actionElement, {
+        [actionTrigger]: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+          setOpen(true);
+          e.stopPropagation();
+        },
+      })}
+
+      {open && (
+        <Position position="absolute" bottom="-30px" ref={dropdownRef}>
+          <Card
+            width={"min-content"}
+            background="white"
+            onMouseLeave={(e) => {
+              e.preventDefault();
+              setOpen(false);
+            }}
+          >
+            {content}
+          </Card>
+        </Position>
+      )}
+    </Position>
+  );
+};
