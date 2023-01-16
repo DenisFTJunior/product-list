de6e04fb569df2b36fbb89218f9b95a7a13e2b66
Denis Junior <junior.denis2354@outlook.com>
2023-01-16 14:00:01 -0300

    fix: left tooltip

--- src/components/base/structure/Tooltip.tsx
+++ src/components/base/structure/Tooltip.tsx
@@ -2,17 +2,20 @@ import React, { Children, useRef } from "react";
 import { ALMOST_WHITE, GREEN } from "../../../toolbox/constants/colors";
 import { Position } from "../../plastic/layout/Position";
 import { Card } from "../../plastic/structure/Card";
+import { Typography } from "../../plastic/structure/Typography";
 
 type TooltipProps = {
   children: React.ReactElement;
   position?: "left" | "right" | "top" | "bottom";
   title: string;
+  width?: string;
 };
 
 export const Tooltip = ({
   children,
   title,
   position = "right",
+  width,
 }: TooltipProps) => {
   const [open, setOpen] = React.useState(false);
   const childrenRef = useRef<HTMLDivElement>(null);
@@ -20,29 +23,40 @@ export const Tooltip = ({
   const formattedPosition = isX
     ? {
         top: `-${(childrenRef.current?.offsetHeight || 1) / 4}px`,
-        [position]: `calc(-${childrenRef.current?.offsetWidth}px - 20px)`,
+        [position === "left"
+          ? "right"
+          : "left"]: `calc(${childrenRef.current?.offsetWidth}px + 20px)`,
       }
     : {
-        left: `-${(childrenRef.current?.offsetWidth || 1) / 2}px`,
-        [position]: `calc(-${childrenRef.current?.offsetHeight}px - 20px)`,
+        [position === "bottom"
+          ? "top"
+          : "bottom"]: `calc(${childrenRef.current?.offsetHeight}px + 20px)`,
       };
 
   return (
-    <Position position="relative">
+    <Position
+      position="relative"
+      width={width || "max-content"}
+      height="max-content"
+    >
       {React.cloneElement(children, {
         onMouseEnter: () => setOpen(true),
         onMouseOut: () => setOpen(false),
         ref: childrenRef,
       })}
       {open && (
-        <Position position="absolute" {...formattedPosition}>
+        <Position
+          position="absolute"
+          {...formattedPosition}
+          width="min-content"
+        >
           <Card
             background={`${GREEN}`}
             padding="5px 10px"
             height="min-content"
             width="max-content"
           >
-            {title}
+            <Typography element="span" text={title} />
           </Card>
         </Position>
       )}
