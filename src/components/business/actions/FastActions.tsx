14b6c641504e49ed4e75d8aa7b77a34729123186
Denis Junior <junior.denis2354@outlook.com>
2023-01-16 16:31:54 -0300

    refactor: change icon to text

--- src/components/business/actions/FastActions.tsx
+++ src/components/business/actions/FastActions.tsx
@@ -2,6 +2,7 @@ import { MdOutlineAdd } from "react-icons/md";
 import { ALMOST_WHITE } from "../../../toolbox/constants/colors";
 import { MenuButton } from "../../base/actions/MenuButton";
 import { Dropdown } from "../../base/structure/Dropdown";
+import { Typography } from "../../plastic/structure/Typography";
 
 const FAST_ACTIONS = [
   {
@@ -13,11 +14,21 @@ export const FastActions = () => {
   return (
     <Dropdown
       actionElement={
-        <MdOutlineAdd
-          size="22px"
-          color={ALMOST_WHITE}
-          style={{ border: `1px solid ${ALMOST_WHITE}`, borderRadius: "2px" }}
-        />
+        <span
+          style={{
+            border: `1px solid ${ALMOST_WHITE}`,
+            padding: "2px 8px",
+            borderRadius: "2px",
+          }}
+        >
+          <Typography
+            text="+"
+            element="span"
+            color={ALMOST_WHITE}
+            fontWeight={500}
+            fontSize="20px"
+          />
+        </span>
       }
       tooltip={{ title: "fast actions", forceSpace: "40px", position: "left" }}
       content={
