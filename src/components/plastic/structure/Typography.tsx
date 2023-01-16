912552d9311e0904b2298fd0109a0d72153a8d2e
Denis Junior <junior.denis2354@outlook.com>
2023-01-16 14:12:19 -0300

    refactor: extra props for typo  whiteSpace | textAlign

--- src/components/plastic/structure/Typography.tsx
+++ src/components/plastic/structure/Typography.tsx
@@ -6,7 +6,12 @@ import { ALMOST_BLACK } from "../../../toolbox/constants/colors";
 type TypographyProps = {
   element: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
   text: string;
-} & Partial<Pick<CSSProperties, "fontSize" | "fontWeight" | "color">>;
+} & Partial<
+  Pick<
+    CSSProperties,
+    "fontSize" | "fontWeight" | "color" | "whiteSpace" | "textAlign"
+  >
+>;
 
 export const Typography = ({
   element,
@@ -14,12 +19,16 @@ export const Typography = ({
   fontSize,
   fontWeight,
   color,
+  whiteSpace,
+  textAlign,
 }: TypographyProps) => {
   const Element = element as keyof JSX.IntrinsicElements;
   const style = css({
     fontSize: fontSize || "1rem",
     fontWeight: fontWeight || "normal",
     color: color || ALMOST_BLACK,
+    textAlign: textAlign || "center",
+    whiteSpace: whiteSpace || "normal",
   });
   return <Element css={style}>{text}</Element>;
 };
