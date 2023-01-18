961bae8fe3985c7b7a333fcf673838bb03734a87
Denis Junior <junior.denis2354@outlook.com>
2023-01-18 16:14:26 -0300

    refactor: dispatch login actions from login screen

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,22 +1,43 @@
 /** @jsxImportSource @emotion/react */
 
+import { useEffect } from "react";
 import { css } from "@emotion/react";
-import { LoginButton } from "../../components/base/actions/LoginButton";
+import { useNavigate } from "react-router-dom";
+import { LoginButton } from "../../components/business/actions/login/LoginButton";
 import { TextInput } from "../../components/base/form/TextInput";
 import { Logo } from "../../components/business/presentation/Logo";
 import { TextButton } from "../../components/plastic/action/TextButton";
-import { Flex, FlexItem } from "../../components/plastic/layout/Flex";
+import { Flex } from "../../components/plastic/layout/Flex";
 import {
   TemplateGrid,
   TemplateGridItem,
 } from "../../components/plastic/layout/grid/TemplateGrid";
 import { Typography } from "../../components/plastic/structure/Typography";
-import { GREEN_BLUE } from "../../toolbox/constants/colors";
+import { ALMOST_BLACK, GREEN_BLUE } from "../../toolbox/constants/colors";
+import { useAppDispatch, useAppSelector } from "../../toolbox/stores/hooks";
+import {
+  fillPassword,
+  fillUsername,
+  login,
+  validateForm,
+} from "../../toolbox/stores/login/actions";
 
 export const LoginScreen = () => {
-  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+  const dispatch = useAppDispatch();
+  const { form, logged } = useAppSelector((state) => state.login);
+
+  const navigate = useNavigate();
+
+  const onSubmit = () => {
+    dispatch(validateForm());
+    dispatch(login());
   };
+
+  useEffect(() => {
+    if (!form.hasError && logged) {
+      navigate("/dashboard");
+    }
+  }, [logged, navigate]);
 
   return (
     <div css={{ width: "100vw", height: "100vh" }}>
@@ -32,12 +53,28 @@ export const LoginScreen = () => {
             alignItems="flex-start"
           >
             <Logo />
-            <TextInput placeholder="Username" />
-            <TextInput placeholder="Password" />
+            <TextInput
+              placeholder="Username"
+              onChange={(e) => dispatch(fillUsername(e.target.value))}
+              value={form.username}
+            />
+            <TextInput
+              placeholder="Password"
+              onChange={(e) => dispatch(fillPassword(e.target.value))}
+              value={form.password}
+              type="password"
+            />
             <Flex justifyContent="space-between" height={"max-content"}>
-              <TextButton text="forgot password" sm={{size:"20px"}}/>
-              <LoginButton onClick={handleSubmit} />
+              <TextButton
+                text="forgot password"
+                sm={{ size: "20px" }}
+                color={`${ALMOST_BLACK}98`}
+              />
+              <LoginButton onClick={onSubmit} />
             </Flex>
+            {form.hasError && (
+              <Typography text={form.errorMessage} color="red" element="span" />
+            )}
           </Flex>
         </TemplateGridItem>
         <TemplateGridItem gridArea="background">
