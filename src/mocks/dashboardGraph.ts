6d9f090b9d0ef0b6f7eb32f84581a230244f8f53
Denis Junior <junior.denis2354@outlook.com>
2023-01-18 16:54:02 -0300

    create: mocks for dashboard

---
+++ src/mocks/dashboardGraph.ts
@@ -0,0 +1,10 @@
+export const productsByGroup = [
+  { name: "Group A", value: 1 },
+  { name: "Group B", value: 2 },
+];
+export const salesInMonth = [
+  { name: "Week1", sales: 1 },
+  { name: "Week2", sales: 2 },
+  { name: "Week3", sales: 4 },
+  { name: "Week4", sales: 3 },
+];
