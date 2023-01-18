8f005fda193f3a0765bd88b01a6bd7b808e41839
Denis Junior <junior.denis2354@outlook.com>
2023-01-18 11:22:29 -0300

    refactor: change gradient colors to enable hover colors

--- src/toolbox/constants/colors.ts
+++ src/toolbox/constants/colors.ts
@@ -3,5 +3,7 @@ export const ALMOST_BLACK = "#1A1A1A";
 export const GREEN = "#57B8B6";
 export const BLUE = "#34446D";
 
-export const BLUE_GREEN = "linear-gradient(180deg, #34446D 0%, #57B8B6 100%)";
-export const GREEN_BLUE = "linear-gradient(180deg, #57B8B6 0%, #34446D 100%)";
+export const BLUE_GREEN =
+  "linear-gradient(180deg, #34446D, transparent) #57B8B6";
+export const GREEN_BLUE =
+  "linear-gradient(180deg, #57B8B6, transparent) #34446D";
