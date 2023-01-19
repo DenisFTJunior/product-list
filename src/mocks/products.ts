023995efa5c7265274d63af7366cafe64cf78743
Denis Junior <junior.denis2354@outlook.com>
2023-01-18 22:46:40 -0300

    create: products mock

---
+++ src/mocks/products.ts
@@ -0,0 +1,30 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 100,
+    group: "Group A",
+    description: "product 1 description",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 200,
+    group: "Group A",
+    description: "product 2 description",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 300,
+    group: "Group B",
+    description: "product 3 description",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    price: 400,
+    group: "Group B",
+    description: "product 4 description",
+  },
+];
