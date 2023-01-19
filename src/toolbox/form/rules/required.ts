88f41ea9aa6c35f7dd812c0478ef58c75576e3b8
Denis Junior <junior.denis2354@outlook.com>
2023-01-19 11:39:10 -0300

    refactor: add number valitor at rule required

--- src/toolbox/form/rules/required.ts
+++ src/toolbox/form/rules/required.ts
@@ -1,6 +1,7 @@
 export const required = (value: any) => {
   if (typeof value === "object") return Object.keys(value).length > 0;
   else if (typeof value === "string") return value.trim().length > 0;
+  else if (typeof value === "number") return value > 0;
   else if (Array.isArray(value)) return value.length > 0;
   return value !== undefined && value !== null;
 };
